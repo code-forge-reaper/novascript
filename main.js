dca06e83e342643c01437e8b67add4cfdb6ab0d8
cross-sniper <cts.contact.br@gmail.com>
2025-07-14 07:08:33 -0300

    feat(core): updated default runner
    - Default runner now includes a js-import-handler

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 import { Interpreter } from './NovaScript.js'; // assuming your interpreter is saved as nova.js
-import fs from 'fs';
+//import fs from 'fs';
+import { readFileSync } from 'fs';
+
 const arg = process.argv[2]||"main.nova"
-const code = fs.readFileSync(arg).toString()
+const code = readFileSync(arg).toString()
 const interpreter = new Interpreter(code);
-interpreter.globals.define("isTesting", ()=>{
-	return process.env.TESTING === "true"
+
+interpreter.globals.define("js-import-handler", (name)=>{
+	try{// since node for some fucking reason doesn't let you use both import and require, you have to use [bun](https://bun.sh/)
+		return require(name)
+	}catch(e){
+		console.log(e)
+		return null
+	}
 })
+
 interpreter.interpret();
