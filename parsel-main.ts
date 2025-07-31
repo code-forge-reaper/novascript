934a2fbf013285087c108d7ee80adcd6046ca650
cross-sniper <cts.contact.br@gmail.com>
2025-07-31 12:48:04 -0300

    now parsel-main.ts also has nova's os import handler

--- parsel-main.ts
+++ parsel-main.ts
@@ -1,13 +1,38 @@
 #!/usr/bin/env node
+import path from "node:path";
+import { fileURLToPath } from "node:url"
+import { createRequire } from "node:module"
 import {
 	ParselParser,
 	ParselRuntime
 } from "./parsel.ts";
 
+// resolve __dirname (TS/ESM doesn't have __dirname)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-// ---- bootstrap ----
-const parser = new ParselParser(process.argv[2] || "main.par");
-const ast = parser.parse();
-const rt = new ParselRuntime(ast);
-rt.parse();
-rt.play();
+const f = path.resolve(process.argv[2] || "main.nova");
+
+// create a require function based on cwd, not this file
+const requireFromCWD = createRequire(path.join(process.cwd(), 'noop.js'))
+
+try {
+	const parser = new ParselParser(f);
+	const interp = new ParselRuntime(parser.parse())
+
+	interp.context.define("os-import-handler", (name: string) => {
+		try {
+			return requireFromCWD(name)
+		} catch (e) {
+			console.error(e)
+			return null
+		}
+	})
+
+	interp.parse();
+
+	interp.play();
+} catch (e) {
+	console.log((e as Error).message)
+}
+
