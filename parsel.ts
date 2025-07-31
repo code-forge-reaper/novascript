e1eaeba4366e2b67adb2e943d8462240f51b16ba
cross-sniper <cts.contact.br@gmail.com>
2025-07-31 12:50:06 -0300

    parsel now doesn't print the result of a os import

--- parsel.ts
+++ parsel.ts
@@ -1480,7 +1480,6 @@ export class ParselRuntime {
                         }
                         const res = this.context.get("os-import-handler", s)(s.path)
                         let name = s.alias || s.path;
-                        console.log(res)
 
                         ctx.define(name, res)
                         break
